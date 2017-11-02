3947e60e9a8fc8522ffbc9084635a711118bdfc0
josh <josh@mulethief.com>
2017-11-02 15:13:20 -0600

    Removed all the drawing stuff to just mess with the vectors. Seems to be working appropriately at the moment.

--- source/main.cpp
+++ source/main.cpp
@@ -5,8 +5,6 @@
 #include <malloc.h>
 #include <time.h>
 
-std::vector<u8*> screenArray;
-
 std::vector<std::vector<u8>> screenArr;
 
 std::vector<u8> funkyBuffer(240*320*3, 0x7F);
@@ -15,47 +13,20 @@ u8 whiteBuffer[240*320*3];
 
 unsigned int zindex = 0;
 
-//u8* mappedMemory;
-
-
-void drawPixel(int x, int y, u8* color) {
-	//screenArray.at(zindex)[(x*240*3 - y*3)] = color[0];
-	//screenArray.at(zindex)[(x*240*3 - y*3)+1] = color[1];
-	//screenArray.at(zindex)[(x*240*3 - y*3)+2] = color[2];
-	//screenArr.at(zindex).at((x*240*3 - y*3)) = color[0]; 
-	screenArr.at(0)[x*240*3 - y*3] = color[0];
-	screenArr.at(0)[x*240*3 - y*3 + 1] = color[1];
-	screenArr.at(0)[x*240*3 - y*3 + 2] = color[2];
-	//printf("Size: %d\n", screenArr.at(0).size());
-}
-
-void drawBlock(int x, int y, u8* color) {
-
-	drawPixel(x,y, color);
-	drawPixel(x,y+1, color);
-	drawPixel(x+1, y, color);
-	drawPixel(x+1, y+1, color);
-
-}
-
 u8 color[3] = {(u8)0x7F, 0, (u8)0x7F};
 
 int main(int argc, char **argv) {
 	gfxInitDefault();
 	consoleInit(GFX_TOP, NULL);
-	consoleDebugInit(debugDevice_CONSOLE);
 
 	gfxSetDoubleBuffering(GFX_BOTTOM, true);
 
 	memset(whiteBuffer, 0xFF, sizeof(whiteBuffer));
 
-	screenArray.push_back(whiteBuffer);
-
 	screenArr.push_back(funkyBuffer);
 
 
 	while (aptMainLoop()) {
-		clock_t start = clock();
 		hidScanInput();
 		u32 kDown = hidKeysDown();
 
@@ -80,47 +51,25 @@ int main(int argc, char **argv) {
 		}
 
 		if (kDown & KEY_A) {
-			// C++ requires casts for malloc.
-			//u8* arr = (u8*)malloc(240*320*3);
-			//memset(arr, 0, 240*320*3);
-			//screenArray.push_back(arr);
-			//std::vector<u8> newBuf(240*320*3, 255);
-			//screenArr.push_back(newBuf);
 			screenArr.push_back(std::vector<u8>(240*320*3, 255));
 			printf("Allocated array\n");
 		}
 
 		if (kDown & KEY_B) {
-			// For fun to see if the function below actually works 
-			// (mappableSpaceFree)
-			//mappedMemory = static_cast<u8*>(mappableAlloc(0x2000));
 		}
 
 		printf("\x1b[0;0HIndex: %d", zindex);
 		printf("\x1b[1;0HArraySize: %d", screenArr.size());
-		// Max is apparently around 93MB, with each screen 250k or so.
-		//printf("\x1b[2;0HUsed memory: %dkB", screenArray.size() * 240 * 320 * 3 / 1024);
 		printf("\x1b[2;0HUsed memory: %dkB\n", screenArr.size() * 240 * 320 * 3 / 1024);
-		//printf("\x1b[3;0HFree Mappable: %d", mappableSpaceFree());
-
-		// Need to fix the arrays this references or it crashes.
-		drawBlock(touch.px, touch.py, color);
 
 		u8* fb = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL);
-		printf("Got FB\n");
-		//memcpy(fb, screenArray[zindex], 240*320*3);
-		//std::copy(screenArr.at(0).begin(), screenArr.at(0).end(), fb );
-		memcpy(fb, &screenArr[0], 240*320*3);
-
-		printf("copied fb\n");
-		clock_t end = clock();
-		printf("\x1b[5;0HLoop time: %f", (end-start)/CLOCKS_PER_SEC * 1000.0);
-
+		printf("\x1b[3;0Hfb location: %d", fb);
+		printf("\x1b[4;0Harray location: %d", &screenArr[0]);
+		printf("\x1b[5;0Harray value: %d", screenArr.at(zindex).at(touch.px));
+		std::copy(screenArr.at(zindex).begin(), screenArr.at(zindex).end(), fb);
 
 		gfxFlushBuffers();
 		gfxSwapBuffers();
-		printf("swapped\n");
-
 		gspWaitForVBlank();
 	}
 
